303f997116ac11623e579f616819a87ba448d85c
Phillip Makowski <philtm@outlook.com>
2022-08-30 16:29:03 -0700

    - detect byte order from c++ 20 endian if available - read ByteOrder.hpp template from a file

--- tools/generate_byte_order_header.cpp
+++ tools/generate_byte_order_header.cpp
@@ -1,39 +1,7 @@
 #include <cstdint>
 #include <cstdio>
+#include <fstream>
 #include <string>
-
-const std::string byte_order_header = R"(
-#pragma once
-
-namespace byte_order {
-#if defined(__BYTE_ORDER__) && defined(__ORDER_LITTLE_ENDIAN__) && __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
-# define BYTE_ORDER_NATIVE_BYTE_ORDER little
-#elif defined(__BYTE_ORDER__) && defined(__ORDER_BIG_ENDIAN__) && __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
-# define BYTE_ORDER_NATIVE_BYTE_ORDER big
-#elif defined(__LITTLE_ENDIAN__)
-# define BYTE_ORDER_NATIVE_BYTE_ORDER little
-#elif defined(__BIG_ENDIAN__)
-# define BYTE_ORDER_NATIVE_BYTE_ORDER big
-#elif defined(_MSC_VER) || defined(__i386__) || defined(__x86_64__)
-# define BYTE_ORDER_NATIVE_BYTE_ORDER little
-#else
-#define DETECTED_NATIVE_BYTE_ORDER %s
-#if !defined(BYTE_ORDER_NATIVE_BYTE_ORDER)
-#define BYTE_ORDER_NATIVE_BYTE_ORDER DETECTED_NATIVE_BYTE_ORDER
-#endif
-#endif
-
-enum class ByteOrder
-{
-    little,
-    big,
-    native = BYTE_ORDER_NATIVE_BYTE_ORDER
-};
-} // namespace byte_order {
-
-#undef DETECTED_NATIVE_BYTE_ORDER
-#undef BYTE_ORDER_NATIVE_BYTE_ORDER
-)";
 
 auto detect_byte_order() -> std::string
 {
@@ -57,6 +25,16 @@ auto detect_byte_order() -> std::string
 
 auto main() -> int
 {
+    const std::string file{"./etc/ByteOrder.hpp.in"};
+    std::ifstream fin(file);
+    if (!fin) {
+        return -1;
+    }
+
+    const std::string byte_order_header(
+            std::istreambuf_iterator<char>{fin},
+            std::istreambuf_iterator<char>{});
+
     auto byte_order = detect_byte_order();
     std::printf(byte_order_header.c_str(), byte_order.c_str());
 
